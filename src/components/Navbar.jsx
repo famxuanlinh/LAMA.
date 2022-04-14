d4de6564d6f8bade6ce22c1856d39b2325815f58
famxuanlinh <famxuanlinh@gmail.com>
2022-04-14 23:16:10 +0700

    slider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -8,28 +9,52 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 `;
 const Left = styled.div`
     flex: 1;
-    cursor: poiter;
+    display: flex;
+    align-items: center;
 `;
 
 const SearchContainer = styled.div`
-    border: 1px solid lightgray;
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
 `;
+
+const Input = styled.input`
+    border: none;
+`
 
 const Language = styled.span`
     font-size:  14px;
 `;
- 
+
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
 const Center = styled.div`
     flex: 1;
+    text-align: center;
 `;
 
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`
 
 const Navbar = () => {
     return (
@@ -37,13 +62,21 @@ const Navbar = () => {
             <Wrapper>
                 <Left>
                     <Language>EN</Language>
-                    <SearchContainer>   
-                        input
-                        <Search/>
+                    <SearchContainer>
+                        <Input />
+                        <Search style={{color:"gray", fontSize:16}}/>
                     </SearchContainer>
                 </Left>
-                <Center>Center <i className="fa-solid fa-magnifying-glass"></i></Center>
-                <Right>Right <i className="fa-solid fa-address-book"></i></Right>
+                <Center><Logo>LAMA.</Logo> <i className="fa-solid fa-magnifying-glass"></i></Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="secondary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     )
